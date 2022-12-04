ae9738cb7cebfa3d51c2083fcf9e2ccd4e073c93
Shahinur Rahman Shahin <srshahin111@gmail.com>
2022-12-04 09:16:34 +0600

    Update like landing page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-let x = document.getElementById("main");
+//sticky header
+window.addEventListener('scroll', () =>{
+    if(window.pageYOffset > 160){
+        document.getElementById('main').setAttribute('class', 'sticky');
+    }
+    else{
+        document.getElementById('main').removeAttribute('class', 'sticky');
+    }
+});
+
+//scroll effect
+// function scrolling(){
+//     window.scrollTo(0, 650);
+// }
+// const serve = document.getElementById('serve');
+// serve.addEventListener('click', scrolling);
